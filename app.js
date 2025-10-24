7d16ffdada445af29742ad5947a10a25fd9d385a
PogodaIgor <igor.pogoda2010@wp.pl>
2025-10-24 15:13:22 +0200

    praca domowa

---
+++ app.js
@@ -0,0 +1,12 @@
+int num = 0
+
+
+function dodac(){
+    document.getElementById("dodac").innerHTML num = num+1
+    liczba + num
+}
+
+function odjac(){
+    document.getElementById("odjac").innerHTML num = num-1
+    liczba - num
+}
