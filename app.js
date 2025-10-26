55a01bfc7f52a77a8854a937c95f1c07cdf710ff
PogodaIgor <igor.pogoda2010@wp.pl>
2025-10-26 19:23:01 +0100

    a

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ let licznik = 0;
 function Dodac() {
   licznik = licznik + 1;
   document.getElementById("licznik").innerHTML = licznik;
+  if (licznik < 0) {
+    document.getElementById("licznik").style.color = "red";
+  } else if (licznik > 0) {
+    document.getElementById("licznik").style.color = "green";
+  } else {
+    document.getElementById("licznik").style.color = "black";
+  }
+  if (licznik == 67) {
+    document.getElementById("liczknik").innerHTML = "SIX SEVEN!!!!!!";
+  }
 }
 function Odjac() {
   licznik = licznik - 1;
   document.getElementById("licznik").innerHTML = licznik;
+  if (licznik < 0) {
+    document.getElementById("licznik").style.color = "red";
+  } else if (licznik > 0) {
+    document.getElementById("licznik").style.color = "green";
+  } else {
+    document.getElementById("licznik").style.color = "black";
+  }
+  if (licznik == 67) {
+    document.getElementById("liczknik").innerHTML = "SIX SEVEN!!!!!!";
+  }
 }
